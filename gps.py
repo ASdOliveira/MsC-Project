d567a181b008d4a3cecad609c0c0767a11e778bd
ASdOliveira <arysson.silva@hotmail.com>
2020-01-21 15:02:06 -0300

    Updating to class structure

--- gps.py
+++ gps.py
@@ -7,16 +7,50 @@
 
 import serial
 import pynmea2
- 
+
+#serial port to connect with GPS board
 port = "/dev/serial0"
- 
-def parseGPS(str):
-    if str.find('GGA') > 0:
-        msg = pynmea2.parse(str)
-        print "Timestamp: %s -- Lat: %s %s -- Lon: %s %s -- Altitude: %s %s -- Satellites: %s" % (msg.timestamp,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units,msg.num_sats)
- 
- 
-serialPort = serial.Serial(port, baudrate = 9600, timeout = 0.5)
-while True:
-    str = serialPort.readline()
-    parseGPS(str)
+
+class GPS():
+    def __init__(self):
+        self.TimeZone = -3
+        #main infos returned from GPS board
+        self.Timestamp = "0"
+        self.Lat = "0"
+        self.Lon = "0"
+        self.Altitude = "0"
+        self.Satellites = "0"
+        self.Timestamp = "0"
+
+        global serialPort
+        #Initializing a serial connection
+        serialPort = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+
+    def updatePosition(self):
+        #Read a line returned from the GPS
+        readline = serialPort.readline()
+        
+        #when GPS lose its position, the return is zero. So, I check if
+        #the info received is zero. If not, is an information.
+        if readline.find('GGA') > 0:
+            msg = pynmea2.parse(readline)
+            self.Timestamp = msg.timestamp
+            
+            if msg.lat_dir == "W":
+                self.Lat = "-" + msg.lat # -8.0000
+            else:
+                self.Lat = "+" + msg.lat # +8.0123
+
+            if msg.lon_dir == "S":
+                self.Lon = "-" + msg.lon
+            else:
+                self.Lon = "+" + msg.lon
+
+            self.Altitude = msg.altitude + msg.altitude_units
+            self.Satellites = msg.num_sats
+            ## OK return
+            return 0 
+
+        else:
+            ## GPS has lose the conection
+            return -1  
--- log.py
+++ log.py
@@ -2,8 +2,13 @@
 
 import time
 
-def Action (text):
-    file = open("Log.txt", "a")
-    time_string = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
-    file.write(time_string + "  "+ text + "\n")
-    file.close()
+class log:
+    def __init__(self,filename):
+        self.filename = filename
+        
+    def logger (self, text):
+        file = open(str(self.filename), "a")
+        time_string = time.strftime("%d/%m/%Y %H:%M:%S", time.localtime())
+        file.write(time_string + "  "+ text + "\n")
+        file.close()
+
